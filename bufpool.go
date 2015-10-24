3dd8e0f3986a503d8853eed7780527f17875b1e0
Karrick S. McDermott <kmcdermott@linkedin.com>
2015-10-24 10:28:15 -0400

    additional examples

--- bufpool.go
+++ bufpool.go
@@ -23,6 +23,32 @@ type BufPool struct {
 
 // New creates a new BufPool instance. The pool size, size of new buffers, and max size of buffers
 // to keep when returned to the pool can all be customized.
+//
+//        package main
+//
+//        import (
+//        	"log"
+//
+//        	"github.com/karrick/bufpool"
+//        )
+//
+//        func main() {
+//        	bp, err := bufpool.New() // can have one or more of PoolSize(), BufferSize, and MaxSize()
+//        	if err != nil {
+//        		log.Fatal(err)
+//        	}
+//        	for i := 0; i < 4*bufpool.DefaultPoolSize; i++ {
+//        		go func() {
+//        			for j := 0; j < 1000; j++ {
+//        				bb := bp.Get()
+//        				for k := 0; k < 3*bufpool.DefaultBufferSize; k++ {
+//        					bb.WriteByte(byte(k % 256))
+//        				}
+//        				bp.Put(bb)
+//        			}
+//        		}()
+//        	}
+//        }
 func New(setters ...func(*BufPool) error) (*BufPool, error) {
 	bp := &BufPool{
 		chSize:  DefaultPoolSize,
@@ -71,6 +97,31 @@ func (bp *BufPool) Put(bb *bytes.Buffer) {
 }
 
 // PoolSize specifies the number of buffers to maintain in the pool.
+//        package main
+//
+//        import (
+//        	"log"
+//
+//        	"github.com/karrick/bufpool"
+//        )
+//
+//        func main() {
+//        	bp, err := bufpool.New(bufpool.PoolSize(25))
+//        	if err != nil {
+//        		log.Fatal(err)
+//        	}
+//        	for i := 0; i < 4*bufpool.DefaultPoolSize; i++ {
+//        		go func() {
+//        			for j := 0; j < 1000; j++ {
+//        				bb := bp.Get()
+//        				for k := 0; k < 3*bufpool.DefaultBufferSize; k++ {
+//        					bb.WriteByte(byte(k % 256))
+//        				}
+//        				bp.Put(bb)
+//        			}
+//        		}()
+//        	}
+//        }
 func PoolSize(size int) func(*BufPool) error {
 	return func(bp *BufPool) error {
 		if size <= 0 {
@@ -82,6 +133,31 @@ func PoolSize(size int) func(*BufPool) error {
 }
 
 // BufferSize specifies the size of newly allocated buffers.
+//        package main
+//
+//        import (
+//        	"log"
+//
+//        	"github.com/karrick/bufpool"
+//        )
+//
+//        func main() {
+//        	bp, err := bufpool.New(bufpool.BufferSize(1024))
+//        	if err != nil {
+//        		log.Fatal(err)
+//        	}
+//        	for i := 0; i < 4*bufpool.DefaultPoolSize; i++ {
+//        		go func() {
+//        			for j := 0; j < 1000; j++ {
+//        				bb := bp.Get()
+//        				for k := 0; k < 3*bufpool.DefaultBufferSize; k++ {
+//        					bb.WriteByte(byte(k % 256))
+//        				}
+//        				bp.Put(bb)
+//        			}
+//        		}()
+//        	}
+//        }
 func BufferSize(size int) func(*BufPool) error {
 	return func(bp *BufPool) error {
 		if size <= 0 {
@@ -95,6 +171,31 @@ func BufferSize(size int) func(*BufPool) error {
 // MaxSize specifies the maximum size of buffers that ought to be kept when returned to the free
 // list.  Buffers with a capacity larger than this size will be discarded, and their memory returned
 // to the runtime.
+//        package main
+//
+//        import (
+//        	"log"
+//
+//        	"github.com/karrick/bufpool"
+//        )
+//
+//        func main() {
+//        	bp, err := bufpool.New(bufpool.MaxSize(32 * 1024))
+//        	if err != nil {
+//        		log.Fatal(err)
+//        	}
+//        	for i := 0; i < 4*bufpool.DefaultPoolSize; i++ {
+//        		go func() {
+//        			for j := 0; j < 1000; j++ {
+//        				bb := bp.Get()
+//        				for k := 0; k < 3*bufpool.DefaultBufferSize; k++ {
+//        					bb.WriteByte(byte(k % 256))
+//        				}
+//        				bp.Put(bb)
+//        			}
+//        		}()
+//        	}
+//        }
 func MaxSize(size int) func(*BufPool) error {
 	return func(bp *BufPool) error {
 		if size <= 0 {
